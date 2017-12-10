888a60497286fdc2c58b851ad31242b7916e0f0f
fweezee <jordangana23@gmail.com>
2017-12-10 14:26:43 +0800

    first

---
+++ Main.py
@@ -0,0 +1,58 @@
+from slackclient import SlackClient
+import time
+import os
+
+token = ("xoxb-283130767090-bY8Sw1ZkHNGGjtiuJsEj5U63")
+
+slack_client = SlackClient(token)
+
+link = '<https://cdn.meme.am/instances/400x/33568413.jpg|That would be great>'
+
+def speechAnalysis(text):
+    return text
+
+
+
+def replyGenerator(text, channel):
+
+    if 'do what' in text.lower() and link not in text:
+        slack_client.api_call(
+            'chat.postMessage',
+            channel=channel,
+            text=link,
+            as_user='true:'
+        )
+
+    if 'suck my dick' in text.lower() and link not in text:
+        slack_client.api_call(
+            'chat.postMessage',
+            channel=channel,
+            text="gladdly",
+            as_user='true:'
+        )
+
+
+
+if slack_client.rtm_connect():
+    print("Connected!!!!")
+
+
+
+
+    while True:
+        events = slack_client.rtm_read()
+        for event in events:
+            if (
+                'channel' in event and
+                'text' in event and
+                event.get('type') == 'message'
+            ):
+                channel = event['channel']
+                text = event['text']
+
+                replyGenerator(text, channel)
+
+        time.sleep(1)
+else:
+    print('Connection failed, invalid token?')
+
